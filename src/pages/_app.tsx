f097ab7de4e91231723c79b1eee2d21f8906c2c4
Kevin Barrios <barrik3@unlv.nevada.edu>
2025-01-28 05:06:00 -0800

    wrap application with the mock data provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import { MockDataProvider } from "@/context/MockDataContext";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return(
+    <MockDataProvider>
+      <Component {...pageProps} />
+    </MockDataProvider>
+  );
 }
