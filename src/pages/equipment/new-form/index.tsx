638a635d9852e0d27119c5ce19d61fa53d204366
Kevin Barrios <barrik3@unlv.nevada.edu>
2025-01-28 03:10:12 -0800

    added theme across pages

--- src/pages/equipment/new-form/index.tsx
+++ src/pages/equipment/new-form/index.tsx
@@ -1,11 +1,24 @@
 import { EquipmentForm } from "@/components/EquipmentForm";
+import { DirectoryList } from "@/components/DirectoryList";
 const Home = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-6xl text-neutral-50 text-bold text-center">
-        New Equipment
-      </h1>
-      <EquipmentForm />
+    <div className="flex flex-col p-4 items-center bg-slate-100 h-auto">
+			<div className="flex flex-col items-center justify-center mt-8 mb-8 space-y-4">
+				<h1 className="text-7xl font-extrabold text-neutral-950 tracking-tight">
+					New Equipment
+				</h1>
+				<p className="text-lg text-neutral-900">
+					Log new equipment.
+				</p>
+				<div className="w-full self-center">
+					<DirectoryList linksToOmit={['/equipment/new-form/']}/>
+				</div>
+				<hr className="w-full border-neutral-300" />
+			</div>
+
+      <div className="bg-slate-300 p-4 rounded-lg self-center">
+        <EquipmentForm />
+      </div>
     </div>
   );
 };
--- src/pages/maintenance/new-record/index.tsx
+++ src/pages/maintenance/new-record/index.tsx
@@ -1,12 +1,24 @@
 import { MaintenanceRecordForm } from "@/components/MaintenanceRecordForm";
+import { DirectoryList } from "@/components/DirectoryList";
 const Home = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-6xl text-neutral-50 text-bold text-center">
-        New Maintenance Record
-      </h1>
+    <div className="flex flex-col p-4 items-center bg-slate-100 h-auto">
+			<div className="flex flex-col items-center justify-center mt-8 mb-8 space-y-4">
+				<h1 className="text-7xl font-extrabold text-neutral-950 tracking-tight">
+					New Maintenance Record
+				</h1>
+				<p className="text-lg text-neutral-900">
+					Log a new maintenance record.
+				</p>
+				<div className="w-2/4 self-center">
+					<DirectoryList linksToOmit={['/maintenance/new-record/']}/>
+				</div>
+				<hr className="w-1/2 border-neutral-300" />
+			</div>
 
-      <MaintenanceRecordForm />
+      <div className="bg-slate-300 p-4 rounded-lg self-center">
+        <MaintenanceRecordForm />
+      </div>
     </div>
   );
 };
