8130573d7142b3ffcd018d5ca38e188b283bb3b4
Kevin Barrios <barrik3@unlv.nevada.edu>
2025-01-31 16:31:59 -0800

    updated maintenance chart to read hours per department

--- src/components/MaintenanceRecordChart.tsx
+++ src/components/MaintenanceRecordChart.tsx
@@ -1,4 +1,3 @@
-import { MaintenanceRecord } from "@/ts/types";
 import {
   BarChart,
   Bar,
@@ -9,13 +8,38 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
+import { useMockData } from "@/context/MockDataContext";
 const MaintenanceRecordChart = () => {
-  const mockData = [
-    { department: "Machining", hoursSpent: 173 },
-    { department: "Assembly", hoursSpent: 203 },
-    { department: "Packaging", hoursSpent: 105 },
-    { department: "Shipping", hoursSpent: 102 },
-  ];
+  const { equipment, maintenanceRecords } = useMockData();
+  const eqIdToDept = equipment.reduce(
+    (acc, eq) => {
+      acc[eq.id] = eq.department;
+      return acc;
+    },
+    {} as Record<string, string>,
+  );
+
+  const deptHours = maintenanceRecords.reduce(
+    (acc, record) => {
+      const dept = eqIdToDept[record.equipmentId];
+      if (!dept) {
+        return acc;
+      }
+      if (!acc[dept]) {
+        acc[dept] = 0;
+      }
+      acc[dept] += record.hoursSpent;
+      return acc;
+    },
+    {} as Record<string, number>,
+  );
+
+  const mockData = Object.keys(deptHours).map((dept) => ({
+    department: dept,
+    hoursSpent: deptHours[dept],
+  }));
+  console.log("maintenance chart mock data");
+  console.log(mockData);
   //fill="#8884d8" - defualt blue from piechart ex
 
   return (
@@ -24,8 +48,11 @@ const MaintenanceRecordChart = () => {
         <ResponsiveContainer width="100%" height="100%">
           <BarChart data={mockData}>
             <CartesianGrid stroke="black" strokeDasharray="3 3" />
-            <XAxis dataKey="department" tick={{fill: 'black', fontSize: 20}}/>
-            <YAxis tick={{fill: 'black', fontSize: 20}}/>
+            <XAxis
+              dataKey="department"
+              tick={{ fill: "black", fontSize: 20 }}
+            />
+            <YAxis tick={{ fill: "black", fontSize: 20 }} />
             <Bar dataKey="hoursSpent" fill="#1e1b4b" />
           </BarChart>
         </ResponsiveContainer>
